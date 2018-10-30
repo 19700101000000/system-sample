00d045cd90d2b14bbc5a978448504478e975f098
Asuka Yagi <yagi.asuka.0x06@gmail.com>
2018-10-31 00:37:58 +0900

    fix default-layout

--- nuxt/layouts/default.vue
+++ nuxt/layouts/default.vue
@@ -1,6 +1,17 @@
 <template lang="pug">
   div
-    b-navbar(toggleable="md", type="dark", variant="primary")
-      b-navbar-brand(href="/") TypeScriptでNuxtに入門する
+    b-navbar(toggleable="md", type="dark", variant="info")
+      b-navbar-toggle(target="nav_collapse")
+      b-navbar-brand(href="#") Top
+      b-collapse#nav_collapse(is-nav)
+        b-navbar-nav
+          b-nav-item(href="#") New
+          b-nav-item(href="#") Hot
+          b-nav-item(href="#") Pickup
+        b-navbar-nav.ml-auto
+          b-nav-item(v-if="false" href="#" right) Signin
+          b-nav-item-dropdown(v-else text="User" right)
+            b-dropdown-item(href="#") Profile
+            b-dropdown-item(href="#") Signout
     nuxt
 </template>
