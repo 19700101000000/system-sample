d7ef37eaadef36e20d8c18df60932a90a61aba5c
Asuka Yagi <yagi.asuka.0x06@gmail.com>
2018-11-13 02:06:13 +0900

    add list.vue

---
+++ nuxt/components/List.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+div
+  b-table(striped hover :items="items.slice(currentPage * 10 - 10, currentPage * 10)")
+  b-pagination-nav(align="center" base-url="#" :number-of-pages="parseInt(items.length / 10 + 1)" v-model="currentPage")
+</template>
+<script>
+export default {
+  props: ['items'],
+  data () {
+    return {
+      currentPage: 1
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+</style>
--- nuxt/pages/buy.vue
+++ nuxt/pages/buy.vue
@@ -2,20 +2,28 @@
 div
   b-tabs
    b-tab(title="受注一覧" active)
-    b-button(href="/order_info" variant="outline-info") じゅちゅう
+    buy-list(:items="items")
+    b-button(href="/order_info" variant="outline-info") 受注
+
    b-tab(title="仕入一覧")
-    b-button(href="/purchase_info" variant="outline-info") しいれ
+    buy-list(:items="items")
+    b-button(href="/purchase_info" variant="outline-info") 仕入
+
    b-tab(title="出荷一覧")
-    b-button(href="/shipment_info" variant="outline-info") しゅっか
+    buy-list(:items="items")
+    b-button(href="/shipment_info" variant="outline-info") 出荷
+
    b-tab(title="請求一覧")
-    b-button(href="/invoice_info" variant="outline-info") せいきゅう
+    buy-list(:items="items")
+    b-button(href="/invoice_info" variant="outline-info") 請求
+
    b-tab(title="回収一覧")
-        b-button(href="/recovery_info" variant="outline-info") かいしゅ
-  logo-default
+    buy-list(:items="items")
+    b-button(href="/recovery_info" variant="outline-info") 回収
 </template>
 
 <script>
-import LogoDefault from '~/components/Logo'
+import BuyList from '~/components/List'
 export default {
   fetch({ store, redirect }) {
     if (!store.state.auth) {
@@ -23,7 +31,37 @@ export default {
     }
   },
   components: {
-    LogoDefault
+    BuyList
+  },
+  data () {
+    return {
+      items: [
+        {name: 'aaa', age: 10},
+        {name: 'bbb', age: 20},
+        {name: 'ccc', age: 30},
+        {name: 'ddd', age: 40},
+        {name: 'eee', age: 50},
+        {name: 'fff', age: 60},
+        {name: 'ggg', age: 10},
+        {name: 'hhh', age: 20},
+        {name: 'iii', age: 30},
+        {name: 'jjj', age: 40},
+        {name: 'kkk', age: 50},
+        {name: 'lll', age: 60},
+        {name: 'mmm', age: 10},
+        {name: 'nnn', age: 20},
+        {name: 'ooo', age: 30},
+        {name: 'ppp', age: 40},
+        {name: 'qqq', age: 50},
+        {name: 'rrr', age: 60},
+        {name: 'sss', age: 10},
+        {name: 'ttt', age: 20},
+        {name: 'uuu', age: 30},
+        {name: 'vvv', age: 40},
+        {name: 'www', age: 50},
+        {name: 'xxx', age: 60},
+      ]
+    }
   }
 }
 </script>
