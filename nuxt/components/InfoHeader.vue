9617bd5c101c5cc47e7aa611026baeb75f368ba1
Asuka Yagi <yagi.asuka.0x06@gmail.com>
2018-11-17 23:51:06 +0900

    fix info-header

--- nuxt/components/InfoHeader.vue
+++ nuxt/components/InfoHeader.vue
@@ -1,11 +1,11 @@
 <template lang="pug">
-b-row.my-1(align-h="between")
+b-row.my-1
   b-col(sm="3")
     b-button(variant="outline-info" block) 戻る
-  b-col(sm="3")
-    b-button(variant="outline-info" block) 戻る
+  b-col(sm="6")
+    b-button(variant="outline-danger" block) 買注文
   b-col(sm="3" v-if="info.next")
-    b-button(:href="info.next.url" variant="outline-info" block) {{info.next.text}}へ
+    b-button(:href="info.next.url" variant="outline-success" block) {{info.next.text}}へ
 </template>
 
 <script>
