9064f8752bcc7ea7141298a1d3525aece7826ac1
高田穂乃花 <kikusui1kaminasizuki0@gmail.com>
2018-11-17 23:58:19 +0900

    server.go追加

--- server/server.go
+++ server/server.go
@@ -20,6 +20,7 @@ func (s *server) serverInit() {
 	s.e.POST("/auth", handler.Auth)
 	s.e.GET("/sample/:num", handler.Sample)
 	s.e.POST("/display/:num", handler.DisplayOrdersTable)
+	s.e.GET("/display/:num", handler.DisplayOrdersTable)
 }
 func (s *server) serverRun() {
 	s.e.Logger.Fatal(s.e.Start(":8080"))
