abaa7f4b6583454c2ed65642632ac70d78e1f33f
Asuka Yagi <yagi.asuka.0x06@gmail.com>
2018-12-01 14:15:42 +0900

    _

--- db/create_db.sql
+++ db/create_db.sql
@@ -1,3 +1,6 @@
 DROP DATABASE IF EXISTS `system_sample`;
 
-CREATE DATABASE `system_sample`;
+CREATE DATABASE `system_sample` CHARACTER SET utf8;
+
+GRANT ALL PRIVILEGES ON `system_sample`.* TO 'server'@'%';
+
--- db/create_table.sql
+++ db/create_table.sql
@@ -1,5 +1,26 @@
 CREATE TABLE `user` (
-    `id`    INTEGER         AUTO_INCREMENT,
-    `name`  VARCHAR(255)    NOT NULL,
+    `id`        INTEGER     AUTO_INCREMENT,
+    `name`      VARCHAR(32) UNIQUE NOT NULL,
+    `show_name` VARCHAR(32) NOT NULL DEFAULT '',
+    `password`  CHAR(64)    NOT NULL,
+    `alive`     BOOLEAN     NOT NULL DEFAULT TRUE,
+    `creator`   BOOLEAN     NOT NULL DEFAULT FALSE,
+
     PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE DEFINER='server'@'%' TRIGGER `user`
+    BEFORE INSERT ON `user` FOR EACH ROW
+        SET NEW.`password` = SHA2(NEW.`password`, 0);
+
+CREATE TABLE `monitor` (
+    `observer`  INTEGER,
+    `target`    INTEGER,
+    `rate`      INTEGER         NOT NULL DEFAULT 0,
+    `review`    VARCHAR(1024)   NOT NULL DEFAULT '',
+
+    PRIMARY KEY(`observer`, `target`),
+    FOREIGN KEY(`observer`) REFERENCES `user`(`id`),
+    FOREIGN KEY(`target`)   REFERENCES `user`(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
