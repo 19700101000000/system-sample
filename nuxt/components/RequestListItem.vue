80aa105dfc8af638e39d98b81604b440de3fdfde
Asuka Yagi <yagi.asuka.0x06@gmail.com>
2019-02-04 11:09:59 +0900

    fix-view

--- nuxt/components/RequestListItem.vue
+++ nuxt/components/RequestListItem.vue
@@ -1,5 +1,14 @@
 <template lang="pug">
-  b-card(:title="value.title") {{ value.description }}
+  b-card(no-body :border-variant="borderVariant")
+    div(slot="header")
+      template(v-if="!value.establish && value.alive") 承認待ち
+      template(v-else-if="value.establish && value.alive") 成立
+      template(v-else-if="value.establish && !value.alive") 終了
+      template(v-else) 破談
+    b-card-body
+      h4 {{ value.title }}
+      p.card-text {{ value.description }}
+    div(slot="footer") JPY {{ value.price }}
 </template>
 
 <script lang="ts">
@@ -23,5 +32,15 @@ interface RequestValue {
 @Component
 export default class RequestListItem extends Vue {
   @Prop() value: RequestValue;
+  public get borderVariant():string {
+    if (!this.value.establish && this.value.alive) {
+      return "primary";
+    } else if (this.value.establish && this.value.alive) {
+      return "success";
+    } else if (this.value.establish && !this.value.alive) {
+      return "secondary"
+    }
+    return "danger";
+  }
 }
 </script>
